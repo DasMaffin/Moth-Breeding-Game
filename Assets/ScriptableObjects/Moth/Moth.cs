e74b78be6edf25ff9340ba12b3e15e6aafd69899
Marvin Lübbers <maffinlp@yahoo.com>
2025-01-03 16:46:23 +0100

    Added Parents Field with MothPair to a moth. Added MothPair to set 2 parents and a chance to get the desired result. Added More moths.

--- Assets/ScriptableObjects/Moth/Moth.cs
+++ Assets/ScriptableObjects/Moth/Moth.cs
@@ -1,7 +1,26 @@
+using System;
 using System.ComponentModel;
 using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class MothPair
+{
+    public Moth FirstMoth;
+    public Moth SecondMoth;
+    public float Chance;
+
+    public MothPair(Moth firstMoth, Moth secondMoth)
+    {
+        FirstMoth = firstMoth;
+        SecondMoth = secondMoth;
+    }
+}
+
 [CreateAssetMenu(fileName = "New Moth", menuName = "Moth/New Moth", order = 1)]
 public class Moth : ScriptableObject
 {
     [Description("The name used in game.")] public string FriendlyName;
+
+    public MothPair[] PossibleParents;
 }
