aef22d392774dd629dd725a83eb7d9a2b9ac5711
Marvin Lübbers <maffinlp@yahoo.com>
2025-01-15 02:45:16 +0100

    Added ability to close the UIs that are hovering over the screen. Added a delegate that calls with a string argument. That new delegate now calls when there was a problem closing a UI, so a text component can catch it and display it to the player. Need to click at least 3 credits to be able to close the credits UI.

--- Assets/Resources/Scripts/Controllers/CreditController.cs
+++ Assets/Resources/Scripts/Controllers/CreditController.cs
@@ -5,11 +5,15 @@ public class CreditController : MonoBehaviour, IPointerClickHandler
 {
     public string webURI;
 
+    private bool wasClicked = false;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         if(!string.IsNullOrEmpty(webURI))
         {
             Application.OpenURL(webURI);
+            CreditsManager.Instance.ClickedCredits += System.Convert.ToInt32(!wasClicked);
+            wasClicked = true;
         }
         else
         {
